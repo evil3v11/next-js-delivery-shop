f8e4f19617aa09baebc6fd7864e757fd7890b6b4
Evil <inc.jackson423@gmail.com>
2026-06-17 16:20:05 +0700

    Added Slider component using Motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <h1>Home page</h1>;
-}
+import Slider from "@/components/slider/Slider";
+
+const Home = () => {
+  return (
+    <main className="w-full mx-auto md-20">
+      <Slider />
+    </main>
+  );
+};
+
+export default Home;
