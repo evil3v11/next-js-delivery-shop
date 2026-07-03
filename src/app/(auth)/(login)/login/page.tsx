380221cef6291605769a3b43591c48e20bb3db45
Evil <inc.jackson423@gmail.com>
2026-07-03 13:23:04 +0700

    small error handling improvement

--- src/app/(auth)/(login)/login/page.tsx
+++ src/app/(auth)/(login)/login/page.tsx
@@ -56,12 +56,14 @@ const LoginPage = () => {
         }),
       });
       const data = await response.json();
-      if (!response.ok) throw new Error(data.error || "Ошибка авторизации");
+      if (!response.ok) throw new Error(data.message || "Ошибка авторизации");
       router.replace("/");
     } catch (error) {
       setError({
         error: error instanceof Error ? error : new Error("Неизвестная ошибка"),
-        userMessage: "Ошибка авторизации, попробуйте снова",
+        userMessage:
+          (error instanceof Error && error.message) ||
+          "Ошибка авторизации, попробуйте снова",
       });
     } finally {
       setIsLoading(false);
